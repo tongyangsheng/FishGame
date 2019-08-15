caadf9644fe523a27b4ee9e2a76f27dd90860267
tongyangsheng <964997115@qq.com>
2019-08-15 17:25:03 +0800

    全局常量，参数定义

---
+++ FishGame/FishGame/Tools/Common.swift
@@ -0,0 +1,31 @@
+//
+//  Common.swift
+//  FishGame
+//
+//  Created by game98 on 2019/8/15.
+//  Copyright © 2019 https://github.com/tongyangsheng. All rights reserved.
+//
+
+import UIKit
+
+//底部的安全距离
+let bottomSafeAreaHeight = UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 0.0
+
+//顶部的安全距离
+let topSafeAreaHeight = (bottomSafeAreaHeight == 0 ? 0 : 24)
+
+//状态栏高度
+let statusBarHeight = UIApplication.shared.statusBarFrame.height;
+
+//导航栏高度
+let navigationHeight: CGFloat = (bottomSafeAreaHeight == 0 ? 64 :88)
+
+//tabbar 高度
+let tabBarHeight = (bottomSafeAreaHeight + 49)
+
+
+//屏幕宽度，高度
+let K_ScreenW = UIScreen.main.bounds.width
+let K_ScreenH = UIScreen.main.bounds.height
+
+
